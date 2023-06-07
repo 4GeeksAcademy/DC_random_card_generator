21778a5a703cd29db70f78d460ff9c87ebcd9d5f
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-06-07 09:39:38 +0000

    Random Card finalizada

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  let arnum = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+  function random(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  let newPalo = random(palos);
+  let newNum = random(arnum);
+
+  let top = document.querySelector(".top");
+  top.innerHTML = newPalo;
+  let bottom = document.querySelector(".bottom");
+  bottom.innerHTML = newPalo;
+
+  if (newPalo === "♦" || newPalo === "♥") {
+    top.style.color = "red";
+    bottom.style.color = "red";
+  }
+
+  let num = document.querySelector(".num");
+  num.innerHTML = newNum;
+
+  // console.log(newNum);
 };
