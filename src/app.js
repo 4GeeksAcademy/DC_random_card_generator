12d3b45ebe84856381526015849ad279703e32ab
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-06-07 10:12:27 +0000

    boton

--- src/app.js
+++ src/app.js
@@ -7,9 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  // ---arrais---
+
   let palos = ["♦", "♥", "♠", "♣"];
 
   let arnum = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+  // ---funcion random---
 
   function random(arr) {
     let randomIndex = Math.floor(Math.random() * arr.length);
@@ -19,6 +24,10 @@ window.onload = function() {
   let newPalo = random(palos);
   let newNum = random(arnum);
 
+  // ---manipulacion del DOM---
+
+  // ---palos---
+
   let top = document.querySelector(".top");
   top.innerHTML = newPalo;
   let bottom = document.querySelector(".bottom");
@@ -27,10 +36,16 @@ window.onload = function() {
   if (newPalo === "♦" || newPalo === "♥") {
     top.style.color = "red";
     bottom.style.color = "red";
+  } else {
+    top.style.color = "black";
+    bottom.style.color = "black";
   }
+
+  // ---numero---
 
   let num = document.querySelector(".num");
   num.innerHTML = newNum;
-
-  // console.log(newNum);
 };
+let btn = document
+  .getElementById("btn-card")
+  .addEventListener("click", window.onload);
